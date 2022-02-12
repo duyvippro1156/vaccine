d7d0cd23e8d4e2d5bc2eede820b38d3bb096471b
duyvippro1156 <maiduybigbang@gmail.com>
2022-02-12 08:57:17 +0700

    update

---
+++ Vaccine-Management-SpringBoot-master/Vaccine-Management-SpringBoot-master/c0720g1-vaccine/src/main/java/com/project/common/MyConstants.java
@@ -0,0 +1,13 @@
+package com.project.common;
+
+public class MyConstants {
+    // Replace with your email here:
+    public static final String MY_EMAIL = "maytao159@gmail.com";
+
+    // Replace password!!
+    public static final String MY_PASSWORD = "Duy12022000";
+
+    // And receiver!
+    public static final String MAIL_RECEIVER= "";
+
+}
